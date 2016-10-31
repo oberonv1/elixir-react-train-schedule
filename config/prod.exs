beb15831fe41d43c2eb11ab326e6d6c702a4a68e
Jonathan Corzo <oberonv1@gmail.com>
2016-10-31 08:30:45 -0400

    added npm webpack build script

--- config/prod.exs
+++ config/prod.exs
@@ -16,19 +16,7 @@ config :mbta, Mbta.Endpoint,
   url: [scheme: "https", host: "elixir-react-train-schedule.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  watchers: [{"node", [
-                     "node_modules/webpack/bin/webpack.js",
-                     "--watch-stdin",
-                     "--colors"
-                   ]},
-                   {"node", [
-                     "node_modules/webpack/bin/webpack.js",
-                     "--watch-stdin",
-                     "--colors",
-                     "--config",
-                     "webpack.server.config.js"
-                   ]}]
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
