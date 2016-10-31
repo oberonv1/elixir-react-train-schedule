2563a7cbbace48e9bfcdb37d7820a7faef9ad33a
Jonathan Corzo <oberonv1@gmail.com>
2016-10-30 20:25:30 -0400

    styles are properly imported to priv/styles/app.css

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,19 +1,21 @@
 defmodule Mbta.PageController do
   use Mbta.Web, :controller
+  require HTTPotion
 
   def index(conn, _params) do
+    response = HTTPotion.get "http://developer.mbta.com/lib/gtrtfs/Departures.csv"
     visitors = Mbta.Visitors.state()
-        initial_state = %{"visitors" => visitors}
-        props = %{
-          "location" => conn.request_path,
-          "initial_state" => initial_state
-        }
+    initial_state = %{"visitors" => visitors}
+    props = %{
+      "location" => conn.request_path,
+      "initial_state" => initial_state
+    }
+    IO.inspect response.body
+    result = Mbta.ReactIO.json_call!(%{
+      component: "./priv/static/server/js/mbta.js",
+      props: props,
+    })
 
-        result = Mbta.ReactIO.json_call!(%{
-          component: "./priv/static/server/js/mbta.js",
-          props: props,
-        })
-
-        render(conn, "index.html", html: result["html"], props: initial_state)
+    render(conn, "index.html", html: result["html"], props: initial_state)
   end
 end
--- web/static/js/containers/App/index.js
+++ web/static/js/containers/App/index.js
@@ -7,14 +7,14 @@ export default class App extends React.Component {
     render() {
         return <div className="container">
             <header className="header">
-            <nav role="navigation">
-            <ul className="nav nav-pills pull-right">
-            <li><a href="http://www.phoenixframework.org/docs">Get Started</a></li>
-        </ul>
-        </nav>
-        <span className="logo"></span>
+                <nav role="navigation">
+                    <ul className="nav nav-pills pull-right">
+                        <li><a href="http://www.phoenixframework.org/docs">Get Started</a></li>
+                    </ul>
+                </nav>
+                <span className="logo"></span>
             </header>
             {this.props.children}
-    </div>;
+        </div>;
     }
 };
