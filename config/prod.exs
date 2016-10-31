4cb8642a1e4702c0304118f48afe847057a8d682
Jonathan Corzo <oberonv1@gmail.com>
2016-10-31 07:45:53 -0400

    added webpack watchers to prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,19 @@ config :mbta, Mbta.Endpoint,
   url: [scheme: "https", host: "elixir-react-train-schedule.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  watchers: [{"node", [
+                     "node_modules/webpack/bin/webpack.js",
+                     "--watch-stdin",
+                     "--colors"
+                   ]},
+                   {"node", [
+                     "node_modules/webpack/bin/webpack.js",
+                     "--watch-stdin",
+                     "--colors",
+                     "--config",
+                     "webpack.server.config.js"
+                   ]}]
 
 # Do not print debug messages in production
 config :logger, level: :info
